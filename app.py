030d12afb10c037c68e6bac9dd5b9f2ea5453a20
Rithujaa Rajendrakumar <rithujaa128@gmail.com>
2026-07-08 15:48:11 -0400

    Fix all LangChain imports for deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from langchain_community.chains import RetrievalQA
 from openai import OpenAI
 import plotly.express as px
 import chromadb
